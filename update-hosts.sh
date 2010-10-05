1a6c183f5b51215172bd9382f6692f0cf981df1c
Jonas Haag <jonas@lophus.org>
2010-10-05 17:21:35 +0200

    shorter diff

--- update-hosts.sh
+++ update-hosts.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
-DIFFER=vimdiff
+
+diffit() {
+    echo $(diff $1 $2 | wc -l) lines changed
+}
 
 cp hosts.winhelp hosts.winhelp.old
 
-echo "Fetching current winhelp hosts..."
+echo Fetching current winhelp hosts...
 wget -O hosts.winhelp http://www.mvps.org/winhelp2002/hosts.txt
 
-echo "Updating local hosts file..."
+echo Updating local hosts file...
 cp hosts.mine hosts
 
 cat hosts.winhelp | grep -v 'localhost' | sed 's/127.0.0.1/127.0.0.0/g' >> hosts
 
-echo "Done, here comes the diff"
-$DIFFER hosts.winhelp.old hosts.winhelp
-rm hosts.winhelp.old
+echo Done, here comes the diff
 
+diffit hosts.winhelp.old hosts.winhelp
+
+rm hosts.winhelp.old
 sudo cp hosts /etc/hosts
