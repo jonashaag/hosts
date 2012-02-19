949a93eb9b54fcd8c3e3a1468fe952b25c422a5f
Claudio Harringer <der.claudio@gmail.com>
2012-02-19 18:48:49 +0100

    Add name given by the hostname command

--- update-hosts.sh
+++ update-hosts.sh
@@ -9,8 +9,16 @@ cp hosts.winhelp hosts.winhelp.old
 echo Fetching current winhelp hosts...
 wget -O hosts.winhelp http://www.mvps.org/winhelp2002/hosts.txt
 
+echo Generating new hosts file...
+rm hosts
+
+echo Adding local hostname...
+echo -n "127.0.0.1 localhost " >> hosts
+hostname >> hosts
+echo >> hosts
+
 echo Updating local hosts file...
-cp hosts.mine hosts
+cat hosts.mine >> hosts
 
 cat hosts.winhelp | tr -d '\r' | grep -v 'localhost' | sed 's/127.0.0.1/127.0.0.0/g' >> hosts
 
